b72a3739eddd05d32e822c0f0ee7041312210613
mtwatso2 <35612911+mtwatso2@users.noreply.github.com>
2023-02-06 15:06:18 -0600

    Update BigFive.py
    FBREF changed some of the tables, so had to redownload all of the data

--- BigFive.py
+++ BigFive.py
@@ -8,7 +8,7 @@ import fifa_funcs as f
 ##### Data for 2017-2018 season #####
 url = 'https://fbref.com/en/comps/Big5/2017-2018/2017-2018-Big-5-European-Leagues-Stats'
 
-links = f.get_all_links(url, lst = [])
+links = f.get_all_links(url)
     
 standard, shooting, passing, misc, pos, defense = f.get_data(links[0])
 
@@ -22,7 +22,7 @@ data17_18 = f.edit_pos(data17_18)
 data17_18['Nation'] = data17_18['Nation'].str.split().str[1] #removing 2 letter abreviation
 data17_18['Comp'] = data17_18['Comp'].str.split(' ', 1).str[1] #removing abbreviation
 
-data17_18 = f.dup_players(data17_18) 
+data17_18 = f.dup_players(data17_18)
 
 data17_18.isna().sum().sum() 
 
@@ -41,16 +41,6 @@ data18_19 = f.edit_pos(data18_19)
 
 data18_19['Nation'] = data18_19['Nation'].str.split().str[1] 
 data18_19['Comp'] = data18_19['Comp'].str.split(' ', 1).str[1]
-
-#lets look at where there is a row with more than one missing column
-
-missing = data18_19.loc[data18_19.isnull().sum(1) > 1]
-
-print(missing)
-
-idx = missing.index #want to drop player with index 1303 due to many missing values 
-
-data18_19 = data18_19.drop(idx) 
 
 data18_19 = f.dup_players(data18_19) 
 
@@ -79,7 +69,7 @@ data19_20.isna().sum().sum()
 data19_20.to_csv('data19_20.csv', index=False)
 
 
-##### Data for 2020-2021 season #####   
+##### Data for 2020-2021 season #####
 standard4, shooting4, passing4, misc4, pos4, defense4 = f.get_data(links[3])
 
 standard4, shooting4, passing4, misc4, pos4, defense4 = f.clean_all(standard4, shooting4, passing4,
@@ -90,7 +80,6 @@ data20_21 = f.merge_all(standard4, shooting4, passing4, misc4, pos4, defense4)
 data20_21 = f.edit_pos(data20_21) 
 
 data20_21['Nation'] = data20_21['Nation'].str.split().str[1]
-
 data20_21['Comp'] = data20_21['Comp'].str.split(' ', 1).str[1]
 
 data20_21 = f.dup_players(data20_21) 
@@ -108,14 +97,19 @@ standard5, shooting5, passing5, misc5, pos5, defense5 = f.clean_all(standard5, s
 
 data21_22 = f.merge_all(standard5, shooting5, passing5, misc5, pos5, defense5) 
 
-data21_22 = f.edit_pos(data21_22) 
+data21_22 = f.edit_pos(data21_22)
 
 data21_22['Nation'] = data21_22['Nation'].str.split().str[1]
-
 data21_22['Comp'] = data21_22['Comp'].str.split(' ', 1).str[1]
 
 data21_22 = f.dup_players(data21_22) 
 
-data21_22.isna().sum().sum()
+data21_22.isnull().sum().sum() #there's a missing value here?
+
+missing = data21_22.loc[data21_22.isnull().sum(1) > 0]
+
+print(missing) # Looks like he's just mising position, but since he only has 5 minutes played, we can remove
+
+data21_22 = data21_22.dropna()
 
 data21_22.to_csv('data21_22.csv', index=False)
